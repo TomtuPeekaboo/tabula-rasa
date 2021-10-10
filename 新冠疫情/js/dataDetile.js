31cc7793e0087543810f86f4dbd5d95b10f05b12
evijidmmkf <3117758508@qq.com>
2021-10-10 12:05:44 +0800

    提交网页

---
+++ 新冠疫情/js/dataDetile.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function () {
+    let peichart1 = document.getElementsByClassName('peichart1')[0];
+    peichart1.style.background = 'conic-gradient(#fff 0%,#fff 20%,#c280ff 20%,#c280ff 64%,#eee 64%,#eee 100%)';
+})
